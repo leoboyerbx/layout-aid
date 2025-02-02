387f2ed615e2ebbb6d451d10629f0069156c56e9
leoboyerbx <contact@leoboyer.dev>
2025-02-02 22:11:13 +0100

    chore: play with config

--- packages/nuxt/playground/nuxt.config.ts
+++ packages/nuxt/playground/nuxt.config.ts
@@ -2,5 +2,13 @@ export default defineNuxtConfig({
     modules: ['../src/module'],
     devtools: { enabled: true },
     compatibilityDate: '2025-01-28',
+    layoutAid: {
+        columns: {
+            count: 14,
+        },
+        outline: {
+            color: 'red',
+        },
+    },
 
 })
